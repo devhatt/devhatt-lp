6c8d8c7f135c06d829f4b0c31255cc31af171aeb
Douglas Murita <douglasb008@gmail.com>
2024-08-17 10:45:21 -0300

    feat: navbar desktop

--- components/petdex/Navbar/Navbar.tsx
+++ components/petdex/Navbar/Navbar.tsx
@@ -12,30 +12,36 @@ function Navbar() {
   const handleButton = () => setIsOpen(o => !o)
 
   return (
-    <div className="">
+    <div className="py-8">
       <header className="flex  justify-between w-[80%] h-20 mx-auto mobile:w-[90%]">
         <div className="flex">
           <nav className='flex items-center mobile:w-[90%] mx-auto '>
-            <div className='w-28 items-center'>
+            <div className='w-28 items-center mr-10'>
               <Logo />
             </div>
-            <ul className="flex space-x-3  mobile:hidden tablet:hidden">
-              <li><a href="#">home</a></li>
-              <li><a href="#">petdex</a></li>
-              <li><a href="#">contato</a></li>
+            <ul className="flex space-x-10  ml-3 mobile:hidden tablet:hidden">
+              <li className='text-petdex-secondaryGreen'>
+                <a className='hover:underline hover:font-semibold' href="#">Home</a>
+              </li>
+              <li className='text-petdex-secondaryGreen'>
+                <a className='hover:underline hover:font-semibold' href="#">Petdex</a>
+              </li>
+              <li className='text-petdex-secondaryGreen'>
+                <a className='hover:underline hover:font-semibold' href="#">Contato</a>
+              </li>
             </ul>
           </nav>
         </div>
         <div className='flex items-center  space-x-10 '>
           {!isOpen &&
-            <div className='flex border-2 border-gray-200 rounded-3xl px-4 py-3 '>
+            <div className='flex border-2 space-x-3 border-gray-200 rounded-3xl px-4 py-3 '>
               <SearchIcon />
 
               <input className='mobile:hidden tablet:hidden' type="search" name="search" placeholder="Pesquise seu amigo aqui!" />
             </div>
           }
           <button
-            className='bg-petdex-secondaryGreen text-white mobile:hidden tablet:hidden '
+            className='hidden bg-petdex-secondaryGreen text-white desktop:block px-10 py-3 rounded-3xl'
           >
             Entrar | Cadastrar
           </button>
