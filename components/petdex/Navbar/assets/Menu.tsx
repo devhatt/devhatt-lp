fecca25baa041150534ebdf4545e03861a11ead0
Douglas Murita <douglasb008@gmail.com>
2024-08-30 17:45:17 -0300

    fix: change tag a to link

--- components/petdex/Navbar/assets/Menu.tsx
+++ components/petdex/Navbar/assets/Menu.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link"
+
 function Menu() {
   return (
     <ul className=" lg:space-x-10 space-y-6 ml-10 lg:space-y-0 lg:ml-3 lg:flex">
       <li className='text-petdex-secondaryGreen'>
-        <a className='hover:underline hover:font-semibold' href="#">Home</a>
+        <Link className='hover:underline hover:font-semibold' href="#">Home</Link>
       </li>
       <li className='text-petdex-secondaryGreen'>
-        <a className='hover:underline hover:font-semibold' href="#">Petdex</a>
+        <Link className='hover:underline hover:font-semibold' href="#">Petdex</Link>
       </li>
       <li className='text-petdex-secondaryGreen'>
-        <a className='hover:underline hover:font-semibold' href="#">Contato</a>
+        <Link className='hover:underline hover:font-semibold' href="#">Contato</Link>
       </li>
     </ul>
   )
